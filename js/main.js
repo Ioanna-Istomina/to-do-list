e8cce30e44c6397449b3c4323bf184be873f1575
Ioanna-Istomina <99349464+Ioanna-Istomina@users.noreply.github.com>
2022-06-02 15:34:11 +0300

    add item list

---
+++ js/main.js
@@ -0,0 +1,66 @@
+const input = document.querySelector("#myInput");
+const addBtn = document.querySelector(".addBtn");
+const list = document.querySelector("#myUL");
+
+addBtn.addEventListener("click", getValue);
+list.addEventListener("click", removeItem);
+
+let items = [];
+getValueStorage();
+
+function getValue() {
+  const value = input.value.trim();
+
+  if (!value) {
+    return;
+  }
+  saveValueInLocalStorage(value);
+  markupItem();
+  input.value = "";
+}
+
+function saveValueInLocalStorage(value) {
+  const item = {
+    id: Date.now(),
+    value,
+    checked: false,
+  };
+
+  items.push(item);
+
+  localStorage.setItem("value", JSON.stringify(items));
+}
+
+function markupItem() {
+  const markup = items
+    .map(
+      (item) =>
+        `<li>${item.value} <span class="close" data-id = ${item.id}>×</span></li>`
+    )
+    .join("");
+  list.innerHTML = markup;
+}
+
+function getValueStorage() {
+  const storageValue = JSON.parse(localStorage.getItem("value"));
+
+  if (!storageValue) {
+    return;
+  }
+  items = storageValue;
+
+  markupItem();
+}
+
+function removeItem(ev) {
+  if (ev.target.classList.contains("close")) {
+    ev.target.parentElement.remove();
+
+    items = items.filter((item) => {
+      console.log(item.id);
+      console.log(ev.target.dataset.id);
+      return item.id !== Number(ev.target.dataset.id);
+    });
+    localStorage.setItem("value", JSON.stringify(items));
+  }
+}
