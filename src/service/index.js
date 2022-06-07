a9ea93689ac18cc448b6984014d07e6e91de6758
Maksym Kundeev <mkundeev@gmail.com>
2022-06-07 16:22:19 +0300

    1

--- src/service/index.js
+++ src/service/index.js
@@ -2,11 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, get, remove, update } from "firebase/database";
 import firebaseConfig from '../const/settings';
 
-
 const app = initializeApp(firebaseConfig);
 const db = getDatabase();
-
-
 
 function saveValueInFirebase(value = '') {
   const id = Date.now()
@@ -18,7 +15,6 @@ function saveValueInFirebase(value = '') {
     try{ set(ref(db, 'items/' + id), item);
     }
     catch(error) {console.log(error)}
- 
 }
 
 function getValueFromFirebase(url) {
